48b6fc9f0aaa4fedef052ddd465783ddfc5af7ba
dudra <dudra@wisc.edu>
2026-02-05 13:52:10 -0600

    minilab1b pipeline fix; signaltap

--- minilab1b/mac.sv
+++ minilab1b/mac.sv
@@ -16,20 +16,26 @@ output reg [DATA_WIDTH*3-1:0] Cout
 // reg [23:0] add_result;
 
 reg[23:0] mult_pipeline;
+reg En_delay;
 
 //Accumulate unit 
 always @(posedge clk, negedge rst_n) begin 
 	if (!rst_n) begin 
 	    mult_pipeline <= {DATA_WIDTH*3{1'b0}};
+		En_delay <= 1'b0;
 		Cout <= {DATA_WIDTH*3{1'b0}};
 	end
 	else if (Clr) begin
 	    mult_pipeline <= {DATA_WIDTH*3{1'b0}};
+		En_delay <= 1'b0;
 		Cout <= {DATA_WIDTH*3{1'b0}};
 	end
-	else if (En) begin
-		mult_pipeline <=  Ain * Bin; 
-		Cout <= Cout + mult_pipeline;
+	else begin
+		if (En_delay) 
+			Cout <= Cout + mult_pipeline; // the insertion of the pipelining causes us to enable which updates Cout
+		En_delay <= En;
+		if (En)
+			mult_pipeline <=  Ain * Bin; 
 	end	
 end
 
