f2a5e6b3eb27553634678b815b6f451626ae3903
dudra <dudra@wisc.edu>
2026-02-12 18:10:16 -0600

    spart.v updated

--- minilab3/spart.v
+++ minilab3/spart.v
@@ -32,26 +32,81 @@ module spart(
     input rxd
 );
 
+// -----------------------------------------
+// Signal Declaration
+// -----------------------------------------
+
 wire tx_en, tx_busy;
 wire rx_baud, rx_ready;
 wire rx_reset_valid;
 wire [7:0] tx_data, rx_data;
-wire [15:0] baud_divisor;
+reg [15:0] baud_divisor;
+wire start;
+reg iorw_flopped;
 
-wire [7:0] status_register = {6'b0, tbr, rda};
+
+
+// -----------------------------------------
+// Combinational Logic
+// -----------------------------------------
 
 assign tbr = ~tx_busy;
 assign rda = rx_ready;
+assign start = ~tx_busy & ~iorw;
 
 assign rx_reset_valid = iocs && iorw && (ioaddr == 2'b00);
 
-assign databus = (iorw  && (ioaddr == 2'b00)) ? rx_data : 
-                 (iorw && (ioaddr == 2'b00)) ? tx_data :
-                 (ioaddr == 2'b01) ? status_register :
-                 (ioaddr == 2'b10) ? baud_divisor[7:0] :
-                 (ioaddr == 2'b11) ? baud_divisor[15:8] :
-                 8'bz;
 
+
+// -----------------------------------------
+// Register Buffers
+// -----------------------------------------
+
+// Baud Divisor Upper
+always @(posedge clk) begin 
+    if (rst)
+        baud_divisor <= 8'h01;
+    else (ioaddr == 2'b11)
+        baud_divisor[15:8] <= databus;
+end
+
+// Baud Divisor Low
+always @(posedge clk) begin 
+    if (rst)
+        baud_divisor[7:0] <= 8'h44;
+    else if (ioaddr == 2'b10)
+        baud_divisor[7:0] <= databus;
+end
+
+// Transmit Buffer (from SPART to Driver)
+always @(posedge clk) begin 
+    if (rst)
+        databus <= 8'h00;
+    else if (iorw_flopped  && (ioaddr == 2'b00))
+        databus <= o_data;
+end
+
+// Receive Buffer (from Driver to SPART)
+always @(posedge clk) begin 
+    if (rst)
+        tx_data <= 8'h00;
+    else if (!iorw_flopped && (ioaddr == 2'b00))
+        tx_data <= databus;
+end
+
+// Status Register
+always @(posedge clk) begin 
+    if (rst)
+        iorw_flopped <= 1'b0;
+    else 
+        iorw_flopped <= iorw;
+end
+
+
+
+// -----------------------------------------
+// Module Instantiations
+// -----------------------------------------
 baud_rate_generator brg(
     .clk        (clk),
     .rst        (rst),
@@ -62,12 +117,13 @@ baud_rate_generator brg(
 );
 
 transmitter tx(
-    .clk        (clk),
-    .rst        (rst),
-    .tx_en      (tx_en),
-    .tx_data    (tx_data),
-    .txd        (txd),
-    .tx_busy    (tx_busy)
+    .clk            (clk),
+    .rst            (rst),
+    .i_tx_en        (tx_en),
+    .i_start        (),
+    .i_data         (tx_data),
+    .o_txd          (txd),
+    .o_busy         (tx_busy)
 );
 
 receiver rx(
